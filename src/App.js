a69ec96959b04b047bc5fd03cad3c28a136c782d
hrdngx <167164434+hrdngx@users.noreply.github.com>
2024-06-05 11:39:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function Square({　　 value, 　onSquareClick }) {
+function Square({　 value, 　onSquareClick }) {
   
   return (
     <button className="square" onClick={onSquareClick}>
@@ -17,31 +17,40 @@ function Board({ xIsNext, squares, onPlay }) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+    
     const nextSquares = squares.slice();
+    
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
     onPlay(nextSquares);
+    
   }
 
   const winner = calculateWinner(squares);
+  
   let status;
+  
   if (winner) {
     status = "Winner: " + winner;
   } else {
+    
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+    
       <div className="status">{status}</div>
       <div className="board-row">
+    
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
+  
       <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
@@ -51,15 +60,20 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+  
       </div>
     </>
   );
 }
 
 export default function Game() {
+  
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  
   const [currentMove, setCurrentMove] = useState(0);
+  
   const xIsNext = currentMove % 2 === 0;
+  
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
